235865b32e40a7f14984f46e4a84ff4773c3e599
Mitchell Millar <mitchsmillar@comcast.net>
2022-12-05 19:27:43 -0800

    Added src/gtag.js for Google Analytics, update package.json app version, and other Display text

--- src/index.js
+++ src/index.js
@@ -15,4 +15,30 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// Try these 2 sendTo*Analytics functions 
+// per (https://create-react-app.dev/docs/measuring-performance/)
+
+function sendToAnalytics(metric) {
+  const body = JSON.stringify(metric);
+  const url = 'https://{example}.com/analytics'; // Change url to fit your analytics url
+
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true });
+  }
+}
+
+function sendToGoogleAnalytics({ id, name, value }) {
+    ga('send', 'event', {
+    	eventCategory: 'Web Vitals',
+    	evebtAction: name,
+    	eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+	eventLabel: id, // id unique to current page load
+	nonInteraction: true, // avoids affecting bounce rate
+    });
+}
+
+reportWebVitals(sendToGoogleAnalytics);
+//reportWebVitals(console.log);
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,8 +4,8 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1>Multi Component React Application</h1>
-                <p>application started from create-react-app script</p>
+                <h1>Tetradigital Enterprises</h1>
+                <p>Boeing AEW&C Project Images</p>
             </div>
         );
     }
